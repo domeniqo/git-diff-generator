0e4ab648712df68430631226707d85347208dd24
Dominik Mlynka <Dominik.Mlynka@Honeywell.com>
2023-05-18 13:57:53 +0200

    basic error handling when process runs implemented - error/successfull message at the end

--- main.py
+++ main.py
@@ -55,30 +55,40 @@ class App:
         button.pack()
 
     def proceed_command(self):
-        repo_path = self.text_box_git_repo.get()
-        output_root_dir = self.text_box_output_dir.get()
-        ref1 = self.text_box_ref1.get()
-        ref2 = self.text_box_ref2.get()
+        try:
+            repo_path = self.text_box_git_repo.get()
+            output_root_dir = self.text_box_output_dir.get()
+            ref1 = self.text_box_ref1.get()
+            ref2 = self.text_box_ref2.get()
 
-        updated_files = path.join(output_root_dir, "updated-" + ref1)
-        original_files = path.join(output_root_dir, "original-" + ref2)
+            updated_files = path.join(output_root_dir, "updated-" + ref1)
+            original_files = path.join(output_root_dir, "original-" + ref2)
 
-        repo = git.Repo(repo_path)
-        full_diff = repo.commit(ref2).diff(ref1)
-        added_files = list(full_diff.iter_change_type("A"))
-        modified_files = list(full_diff.iter_change_type("M"))
-        renamed_files = list(full_diff.iter_change_type("R"))
-        deleted_files = list(full_diff.iter_change_type("D"))
+            repo = git.Repo(repo_path)
+            full_diff = repo.commit(ref2).diff(ref1)
+            added_files = list(full_diff.iter_change_type("A"))
+            modified_files = list(full_diff.iter_change_type("M"))
+            renamed_files = list(full_diff.iter_change_type("R"))
+            deleted_files = list(full_diff.iter_change_type("D"))
 
-        for diff_object in added_files + modified_files + renamed_files + deleted_files:
-            if diff_object.a_blob:
-                final_path = path.abspath(path.join(original_files, diff_object.a_path))
-                os.makedirs(path.dirname(final_path), exist_ok=True)
-                diff_object.a_blob.stream_data(open(final_path, "wb"))
-            if diff_object.b_blob:
-                final_path = path.abspath(path.join(updated_files, diff_object.a_path))
-                os.makedirs(path.dirname(final_path), exist_ok=True)
-                diff_object.b_blob.stream_data(open(final_path, "wb"))
+            for diff_object in (
+                added_files + modified_files + renamed_files + deleted_files
+            ):
+                if diff_object.a_blob:
+                    final_path = path.abspath(
+                        path.join(original_files, diff_object.a_path)
+                    )
+                    os.makedirs(path.dirname(final_path), exist_ok=True)
+                    diff_object.a_blob.stream_data(open(final_path, "wb"))
+                if diff_object.b_blob:
+                    final_path = path.abspath(
+                        path.join(updated_files, diff_object.a_path)
+                    )
+                    os.makedirs(path.dirname(final_path), exist_ok=True)
+                    diff_object.b_blob.stream_data(open(final_path, "wb"))
+            showinfo(title="SUCCESS", message="Task finished successfully")
+        except:
+            showerror(title="ERROR", message="Task failed successfully")
 
 
 if __name__ == "__main__":
