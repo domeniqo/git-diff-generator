af2b40ae940635567ed265c474fa168000f28ab8
Dominik Mlynka <Dominik.Mlynka@Honeywell.com>
2024-03-08 11:36:16 +0100

    selection of action performed by dropdown menu instead of two separate buttons in GUI

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import git
-from os import path
 import os
-from tkinter import *
-from tkinter.messagebox import showerror, showinfo, showwarning, askyesnocancel
-from tkinter.filedialog import askdirectory
 import re
 
+from os import path
+from tkinter import *
+from tkinter.filedialog import askdirectory
+from tkinter.messagebox import showerror, showinfo, showwarning, askyesnocancel
+from tkinter.ttk import Combobox
+
+version="2.2"
 
 class App:
+    
+    options=["Generate ref diff", "Generate merge diff"]
+    
     def run(self):
         self.main_window = Tk()
-        self.main_window.title(f"Git diff folders generator")
+        self.main_window.title(f"Git diff folders generator v{version}")
         self.main_window.protocol("WM_DELETE_WINDOW", self._quit)
 
         self.init_gui()
@@ -27,62 +33,72 @@ class App:
 
     def init_gui(self):
         # row 1
+        label_action = Label(self.main_window)
+        label_action.config(text="Choose action: ")
+        label_action.grid(row=1, column=1, sticky="E")
+        self.dropdown_menu = Combobox(values=self.options, state="readonly")
+        self.dropdown_menu.config(width=50)
+        self.dropdown_menu.grid(row=1, column=2)
+        self.dropdown_menu.current(0)
+        self.dropdown_menu.bind("<<ComboboxSelected>>", self.check_dropdown_change)
+        
+        # row 2
         label_git_repo = Label(self.main_window)
         label_git_repo.config(text="Git repo path: ")
-        label_git_repo.grid(row=1, column=1, sticky="E")
+        label_git_repo.grid(row=2, column=1, sticky="E")
         self.text_box_git_repo = Entry(self.main_window)
         self.text_box_git_repo.config(width=50)
-        self.text_box_git_repo.grid(row=1, column=2)
+        self.text_box_git_repo.grid(row=2, column=2)
         browse_repo_button = Button(self.main_window)
         browse_repo_button.config(text="Browse...")
         browse_repo_button.config(width=10)
         browse_repo_button.config(
             command=lambda: self.fill_folder_path(self.text_box_git_repo)
         )
-        browse_repo_button.grid(row=1, column=3)
+        browse_repo_button.grid(row=2, column=3)
 
-        # row 2
+        # row 3
         label_output_dir = Label(self.main_window)
         label_output_dir.config(text="Output dir: ")
-        label_output_dir.grid(row=2, column=1, sticky="E")
+        label_output_dir.grid(row=3, column=1, sticky="E")
         self.text_box_output_dir = Entry(self.main_window)
         self.text_box_output_dir.config(width=50)
-        self.text_box_output_dir.grid(row=2, column=2)
+        self.text_box_output_dir.grid(row=3, column=2)
         browse_output_dir_button = Button(self.main_window)
         browse_output_dir_button.config(text="Browse...")
         browse_output_dir_button.config(width=10)
         browse_output_dir_button.config(
             command=lambda: self.fill_folder_path(self.text_box_output_dir)
         )
-        browse_output_dir_button.grid(row=2, column=3)
+        browse_output_dir_button.grid(row=3, column=3)
 
-        # row 3
+        # row 4
         label_ref1 = Label(self.main_window)
-        label_ref1.grid(row=3, column=1, sticky="E")
+        label_ref1.grid(row=4, column=1, sticky="E")
         label_ref1.config(
             text="Git ref1 - updated version (branch, tag, commit hash): "
         )
         self.text_box_ref1 = Entry(self.main_window)
         self.text_box_ref1.config(width=50)
-        self.text_box_ref1.grid(row=3, column=2)
+        self.text_box_ref1.grid(row=4, column=2)
 
-        # row 4
+        # row 5
         label_ref2 = Label(self.main_window)
-        label_ref2.grid(row=4, column=1, sticky="E")
+        label_ref2.grid(row=5, column=1, sticky="E")
         label_ref2.config(
             text="Git ref2 - original version (branch, tag, commit hash): "
         )
         self.text_box_ref2 = Entry(self.main_window)
         self.text_box_ref2.config(width=50)
-        self.text_box_ref2.grid(row=4, column=2)
+        self.text_box_ref2.grid(row=5, column=2)
 
-        # row 5
+        # row 6
         label_regex = Label(self.main_window)
-        label_regex.grid(row=5, column=1, sticky="E")
+        label_regex.grid(row=6, column=1, sticky="E")
         label_regex.config(text="Regex to filter out files (match==include): ")
         self.text_box_regex = Entry(self.main_window)
         self.text_box_regex.config(width=50)
-        self.text_box_regex.grid(row=5, column=2)
+        self.text_box_regex.grid(row=6, column=2)
         self.var1 = BooleanVar()
         self.ignore_case = Checkbutton(
             self.main_window,
@@ -91,21 +107,14 @@ class App:
             onvalue=True,
             offvalue=False,
         )
-        self.ignore_case.grid(row=5, column=3, sticky="W")
+        self.ignore_case.grid(row=6, column=3, sticky="W")
 
-        # row 6
-        generate_button = Button(self.main_window)
-        generate_button.config(text="GENERATE REFS DIFF")
-        generate_button.config(width=60)
-        generate_button.config(command=self.generate_refs_diff)
-        generate_button.grid(row=6, column=1, columnspan=3)
-        
         # row 7
-        generate_button2 = Button(self.main_window)
-        generate_button2.config(text="GENERATE MERGE DIFF")
-        generate_button2.config(width=60)
-        generate_button2.config(command=self.generate_merge_diff)
-        generate_button2.grid(row=7, column=1, columnspan=3)
+        generate_button = Button(self.main_window)
+        generate_button.config(text="PROCEED WITH ACTION")
+        generate_button.config(width=60)
+        generate_button.config(command=self.process_command)
+        generate_button.grid(row=7, column=1, columnspan=3)
 
     def fill_folder_path(self, entry_to_fill):
         dir_path = askdirectory(title="Select folder")
@@ -113,6 +122,26 @@ class App:
             entry_to_fill.delete(0, END)
             entry_to_fill.insert(0, dir_path)
 
+    def check_dropdown_change(self, event):
+        
+        action = event.widget.get()
+
+        if action == self.options[0]:
+            self.text_box_ref1.config(state="normal")
+            self.text_box_ref2.config(state="normal")
+            self.text_box_regex.config(state="normal")
+        elif action == self.options[1]:
+            self.text_box_ref1.config(state="disabled")
+            self.text_box_ref2.config(state="disabled")
+            self.text_box_regex.config(state="disabled")
+            
+    def process_command(self):
+        action = self.dropdown_menu.get()
+        if action == self.options[0]:
+            self.generate_refs_diff()
+        elif action == self.options[1]:
+            self.generate_merge_diff()
+            
     def generate_refs_diff(self):
         try:
             repo_path = self.text_box_git_repo.get()
